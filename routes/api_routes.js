0211aaba7f8e0af5e030c2ad0730d15fca4be546
Alla Akiyeva <alla.akiyeva@gmail.com>
2019-07-18 19:46:01 -0400

    handlebars added, routes added

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,28 +1,87 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 
+
 module.exports  = function (app) {
-    app.get("/", (req, res) => {
-        res.send("some text");
+    const db = require("../models/Article");
+    
+    app.get("/scrape", (req, res) => {
+
+        const articlesArr = [];
+
         // Get the html from wsj.com
         axios.get("https://www.wsj.com/news/technology")
-        .then(res => {
+        .then(response => {
          // Turn that html into a jQuery like DOM
-            const $ = cheerio.load(res.data);
+            const $ = cheerio.load(response.data);
+            
+
+            let titlesArr = [];
 
          // Search the DOM using jQuery-like features to find articles
-            // $('#fin-src-res-table td[aria-label="Name"]')
-            
-            $(".wsj-headline a").each((index, title) => {
-                const headline = $(title).text();
-                console.log(headline);
-            });
+            $(".wsj-headline a.wsj-headline-link").each((index, title) => {
+                
+                const article = {};
     
+                article.title = $(title).text();
+                article.link = $(title).attr("href");
 
+                if (titlesArr.indexOf(article.title) == -1 ) {
+                    titlesArr.push(article.title);
+                    articlesArr.push(article);
+                }
+            
+            });
+            
+            db.remove({})
 
-        }).catch(err => console.log(err));    
+            db.create(articlesArr) 
+            .catch(err => {
+                console.log(err);
+            });
 
+            
+
+            console.log(articlesArr);
+            console.log(articlesArr.length);
+
+        });
+
+        res.render('index', articlesArr);
         
+    });
+
+    app.get("/home", (req, res) => {
+        db.find({})
+        .then(article => {
+            res.render('index', {article});
+        }).catch(err => {
+            res.json(err)
+        });
     })
+
+    app.get("/articles", (req, res) => {
+        db.find({})
+        .then(article => {
+             json(article);  
+        }).catch(err => {
+            res.json(err)
+        })
+    });
+
+    // app.get("/articles/:id", (req, res) => {
+    //     db.findOne({_id: req.params.id })
+    //     .populate("note")
+    //     .then(article => {
+    //         res.json(article);
+    //     }).catch(err => {
+    //         res.json(err);
+    //     })
+    // })
+
+    // app.post("/articles/:id", (req, res) =>
+    //     db.
+    // )
+    
 }
 
--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 const PORT = 5000
 const api_routes = require("./routes/api_routes")
 const mongoose = require("mongoose")
-const db = require("")
 
 const app = express()
 
-api_routes(app);
+app.engine("handlebars", exphbs())
+app.set("view engine", "handlebars");
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/unit18Populater", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost/news_db", 
+    {useNewUrlParser: true},
+    function () {
+        console.log("connected to server");
 
-// Routes
+        api_routes(app);
+
+        app.listen(PORT, () => console.log("Listening on PORT %s", PORT))
+    });
 
 
-app.listen(PORT, () => console.log("Listening on PORT %s", PORT))
+
+
+
